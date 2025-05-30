5ea9350438bb17aab8cbf53114e81a0b99fcbb99
Olaf Stroosma <O.Stroosma@tudelft.nl>
2025-05-30 12:06:02 +0200

    Some improvements to the example dueca_mod.py, making optional components explicitly selectable, adding numerous comments, etc.

--- run/solo/solo/dueca_mod.py
+++ run/solo/solo/dueca_mod.py
@@ -1,7 +1,17 @@
 ## -*-python-*-
-## this is an example dueca_mod.py file, for you to start out with and adapt
-## according to your needs. Note that you need a dueca_mod.py file only for the
-## node with number 0
+
+## configure the simulation
+
+# use the replay functionality (advanced)
+use_replay = False
+
+# show the inputs in a graphical way
+show_inputs = False
+
+# entity name for naming channels, modules, logfiles etc.
+entity_name = "ph-simple"
+
+## find the joystick hardware through the SDL library
 try:
     import sdl2
 
@@ -22,25 +32,16 @@ except ModuleNotFoundError:
     print("Cannot import python sdl2 module, assuming joystick 0")
     use_js = 0
 
-## in general, it is a good idea to clearly document your set up
-## this is an excellent place.
-guidev = True
-sticky = 1
-
-# joystick check disabled; affects joystick count
 print(f"Selected joystick {use_js}")
 
 ## node set-up
-ecs_node = 0   # dutmms1, send order 3
-#aux_node = 1   # dutmms3, send order 1
-#pfd_node = 2   # dutmms5, send order 2
-#cl_node = 3    # dutmms4, send order 0
+ecs_node = 0   # only node in solo
 
 ## priority set-up
-# normal nodes: 0 administration
+# normal nodes: 0 administration + graphics
 #               1 simulation, unpackers
 #               2 communication
-#               3 ticker
+#               3 ticker (highest_manager)
 
 # administration priority. Run the interface and logging here
 admin_priority = dueca.PrioritySpec(0, 0)
@@ -48,24 +49,15 @@ admin_priority = dueca.PrioritySpec(0, 0)
 # priority of simulation, just above adiminstration
 sim_priority = dueca.PrioritySpec(1, 0)
 
-# nodes with a different priority scheme
-# control loading node has 0, 1 and 2 as above and furthermore
-#               3 stick priority
-#               4 ticker priority
-# priority of the stick. Higher than prio of communication
-# stick_priority = dueca.PrioritySpec(3, 0)
+## timing set-up
 
-# timing set-up
-# timing of the stick calculations. Assuming 100 usec ticks, this gives 2500 Hz
-# stick_timing = dueca.TimeSpec(0, 4)
-
-# this is normally 100, giving 100 Hz timing
+# regular modules run at 100 Hz timing (100 ticks of 0.1ms)
 sim_timing = dueca.TimeSpec(0, 100)
 
-## for now, display on 50 Hz
+# display should be lower than common (vsync-locked) refresh rate, so 50 Hz
 display_timing = dueca.TimeSpec(0, 200)
 
-## log a bit more economical, 25 Hz
+# log a bit more economical, 25 Hz
 log_timing = dueca.TimeSpec(0, 400)
 
 ## ---------------------------------------------------------------------
@@ -81,17 +73,18 @@ if this_node_id == ecs_node:
     DUECA_mods.append(dueca.Module("log-view", "", admin_priority))
     DUECA_mods.append(dueca.Module("channel-view", "", admin_priority))
 
-    for e in ("ph-simple",):
-        DUECA_mods.append(
-            dueca.Module("initials-inventory", e, admin_priority).param(
-                # reference_file=f"initials-{e}.toml",
-                store_file=f"initials-{e}-%Y%m%d_%H%M.toml"))
-        DUECA_mods.append(
-            dueca.Module("replay-master", e, admin_priority).param(
-                #reference_files=f"recordings-{e}.ddff",
-                store_files=f"recordings-{e}-%Y%m%d_%H%M%S.ddff"))
+    if use_replay:
+        for e in (entity_name,):
+            DUECA_mods.append(
+                dueca.Module("initials-inventory", e, admin_priority).param(
+                    # reference_file=f"initials-{e}.toml",
+                    store_file=f"initials-{e}-%Y%m%d_%H%M.toml"))
+            DUECA_mods.append(
+                dueca.Module("replay-master", e, admin_priority).param(
+                    #reference_files=f"recordings-{e}.ddff",
+                    store_files=f"recordings-{e}-%Y%m%d_%H%M%S.ddff"))
 
-        # create the entity with that list
+    # create the entity with that list
     DUECA_entity = dueca.Entity("dueca", DUECA_mods)
 
 ## ---------------------------------------------------------------------
@@ -103,19 +96,23 @@ if this_node_id == ecs_node:
         "flexi-stick", "", sim_priority).param(
             set_timing = sim_timing,
             check_timing = (10000, 20000),
-            enable_record_replay = True).param(
+            enable_record_replay = use_replay).param(
                 ('add-device', f"tiger:{use_js}"),
-                # values are already coverted to -1 .. 1
+                # values are already -1 .. 1, no conversion would look like this:
                 #('create-poly', ("stickx", "tiger.a[0]")),
                 #('poly-params', (0.0, 1.0)),
+                # instead, let's make some segments
                 ('create-segments', ("stickx", "tiger.a[0]")),
                 ('segments-params',
                  (-1.0, -1.0, -0.2, 0.0, 0.2, 0.0, 1.0, 1.0)),
+                # for the second axis, make it a percentage
                 ('create-poly', ("sticky", "tiger.a[1]")),
-                ('poly-params', (0.0, 1.0)),
+                ('poly-params', (0.0, 100.0)),
+                # for the throttle, let it run from 0 to 1
                 ('create-poly', ("throttle", "tiger.a[2]")),
                 ('poly-params', (0.5, 0.5)),
                 # let the hat up & down control a counter
+                # note that the hats have d(own), u(p), l(eft), and r(ight) switches
                 ('create-counter', ("iflaps", "tiger.d[0]", "tiger.u[0]")),
                 ('counter-params', (0, 3, 0)),
                 # convert the iflap counter to flap values
@@ -126,44 +123,50 @@ if this_node_id == ecs_node:
                 ('create-weighted', ("wsum", "2.0*iflaps", "-1.3*tiger.l[0]")),
 
                 # channel written
+                # note that the data class (DCO) needs to be in some module's
+                # comm-objects.lst for it to be included in the DCO registry
                 ('add-channel',
-                 ("main",                       # var name
-                  "FlexiStickTest://ph-simple", # channel full name
-                  "FlexiStickTest",             # data class
-                  "first try",                  # entry label
-                  "event")),
+                 ("main",                          # var name, for use in add-link
+                  "FlexiStickTest://"+entity_name, # channel full name, convention is DCO://Entity
+                  "FlexiStickTest",                # data class, from registry
+                  "",                              # entry label
+                  "event")),                       # [optional] event channel, leave out for stream
 
                 # link calculated & stick values to channel
-                ('add-link', ("main.valb", "tiger.b[0]")),
-                ('add-link', ("main.vecd[0]", "stickx")),
-                ('add-link', ("main.vecd[1]", "sticky")),
-                ('add-link', ("main.valf", "tiger.a[2]")),
-                ('add-link', ("main.vals", "tiger.b[0]")),
-                ('add-link', ("main.vali", "tiger.h[0]")),
-                ('add-link', ("main.vald", "wsum")),
+                ('add-link', ("main.valb", "tiger.b[0]")), # direct button value
+                ('add-link', ("main.vecd[0]", "stickx")),  # calculated polynomial
+                ('add-link', ("main.vecd[1]", "sticky")),  # calculated polynomial
+                ('add-link', ("main.valf", "tiger.a[2]")), # direct axis value
+                ('add-link', ("main.vals", "tiger.b[0]")), # direct button value
+                # direct hat output is a bitmask (0x1: up, 0x2: right, 0x4: down, 0x8: left)
+                ('add-link', ("main.vali", "tiger.h[0]")), # direct hat value 
+                ('add-link', ("main.vald", "wsum")),       # calculated weighted sum
 
                 ('add-channel',
-                 ("second",
-                  "MediumJoystick://ph-simple",
-                  "MediumJoystick",
-                  "verify",
-                  "event")),
+                 ("second",                        # var name
+                  "MediumJoystick://"+entity_name, # channel full name
+                  "MediumJoystick",                # data class
+                  "")),                            # entry label
 
-                ('add-link', ("second.axis[0]", "stickx")),
-                ('add-link', ("second.axis[1]", "sticky")),
-                ('add-link', ("second.axis[2]", "tiger.a[2]")),
-                ('add-link', ("second.button[0]", "tiger.b[0]")),
+                ('add-link', ("second.axis[0]", "stickx")),       # calculated polynomial
+                ('add-link', ("second.axis[1]", "sticky")),       # calculated polynomial
+                ('add-link', ("second.axis[2]", "tiger.a[2]")),   # direct axis value
+                ('add-link', ("second.button[0]", "tiger.b[0]")), # direct button value
                 )
         )
-    mymods.append(dueca.Module(
-        "stick-view", "", admin_priority).param(
-            set_timing = sim_timing,
-            check_timing = (10000, 20000))
-            )
-    filer = dueca.ReplayFiler("ph-simple", sim_priority)
 
-# etc, each node can have modules in its mymods list
+    # an optional graphical representation of the FlexiStickTest channel
+    if show_inputs :
+        mymods.append(dueca.Module(
+            "stick-view", "", admin_priority).param(
+                set_timing = sim_timing,
+                check_timing = (10000, 20000))
+        )
+
+    # the recorder for the replay functionality, if enabled
+    if use_replay:
+        filer = dueca.ReplayFiler(entity_name, sim_priority)
 
 # then combine in an entity (one "copy" per node)
 if mymods:
-    myentity = dueca.Entity("ph-simple", mymods)
+    myentity = dueca.Entity(entity_name, mymods)
